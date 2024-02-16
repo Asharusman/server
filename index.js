ec35268a2867c70dc1cc48bce2c992f7e8ca1bfd
Asharusman <muhammadasharusman@gmail.com>
2024-02-17 00:19:20 +0500

    feat : goofle and facebook auth feature

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const session = require('express-session');
 const app = express();
 const port = 4000;
 const bodyParser = require('body-parser');
@@ -9,15 +10,37 @@ require('./model/User')
 app.use(bodyParser.json());
 const cors = require("cors");
 const authRoutes = require('./routes/authRoutes')
+const facebookAuthRoutes = require('./routes/facebookAuth')
+const googleAuthRoutes = require('./routes/googleAuth')
 
+const passport = require('passport');
 // app.use(bodyParser.json({ limit: '10mb' }));
 // app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
 
 app.use(cors({ origin: true, credentials: true }));
 app.use(express.json({limit:'10mb'}));
 
-app.use(authRoutes);
+app.use(
+    session({
+      resave: false,
+      saveUninitialized: true,
+      secret: process.env.SESSION_SECRET,
+    })
+  );
+  
+  app.use(passport.initialize());
+  app.use(passport.session());
+  
+  passport.serializeUser(function (user, cb) {
+    cb(null, user);
+  });
+  passport.deserializeUser(function (obj, cb) {
+    cb(null, obj);
+  });
 
+app.use(authRoutes);
+app.use('/auth/facebook', facebookAuthRoutes);
+app.use('/auth/google', googleAuthRoutes);
 // const con =mysql.createConnection({
 //     host:'localhost',
 //     user:'root',
